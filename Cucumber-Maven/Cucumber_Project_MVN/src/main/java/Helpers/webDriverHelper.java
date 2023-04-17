ff86df50e73a6035f37b908e5e920cfe98649f82
Indranilsen09 <indranilsenoff@gmail.com>
2023-04-17 22:26:00 +0530

    ChromeDriver Update

--- Cucumber-Maven/Cucumber_Project_MVN/src/main/java/Helpers/webDriverHelper.java
+++ Cucumber-Maven/Cucumber_Project_MVN/src/main/java/Helpers/webDriverHelper.java
@@ -1,4 +1,27 @@
 package Helpers;
 
-public class webDriverHelper {
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.HashMap;
+import java.util.logging.Level;
+
+public class webDriverHelper
+{
+    private static RemoteWebDriver Real_Driver =null;
+    private static String driverPath="\\tools\\ChromeDriver\\chromedriver.exe";
+
+    public static void launchbrowser()
+    {
+        System.getProperty("webdriver.chrome.driver","user.dir"+driverPath);
+    }
+
+
+
+
+
 }
